7f09c4e0382da11c7dfbac50f5c2f82bf853227b
bifera <jstn.rudnicka@gmail.com>
2020-10-18 12:33:19 +0200

    ADD javascript

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -8,7 +8,8 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./jquery.slim.js";
+import "./bootstrap.bundle.min.js";
 
 
 // Option 2
